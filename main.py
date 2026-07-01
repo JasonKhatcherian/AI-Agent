cd97107c281cd919309036fbb8ef1a6c31c0273d
ThePrimeagen <the.primeagen@aol.com>
2026-07-01 09:52:56 +0300

    B: display prompt and response token counts

--- main.py
+++ main.py
@@ -17,9 +17,16 @@ def main():
     client = genai.Client(api_key=api_key)
 
     print("Gemini client initialized successfully.")
-
+    prompts="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     # You can now use 'client' to call Gemini models
-
+    response = client.models.generate_content(
+    model='gemini-2.5-flash', contents=prompts)
+    p_token=response.usage_metadata.prompt_token_count
+    r_token=response.usage_metadata.candidates_token_count
+    print(prompts)
+    print("Prompt tokens:",p_token)
+    print("Response tokens:",r_token)
+    print(response.text)
 
 if __name__ == "__main__":
     main()

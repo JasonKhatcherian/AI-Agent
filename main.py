69592e748671275d9ad901c00aba909e652e465d
ThePrimeagen <the.primeagen@aol.com>
2026-07-01 10:09:12 +0300

    Accept user prompt from command line

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
-
+import argparse
 def main():
     # Load environment variables from .env file
     load_dotenv()
@@ -17,13 +17,16 @@ def main():
     client = genai.Client(api_key=api_key)
 
     print("Gemini client initialized successfully.")
-    prompts="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    # You can now use 'client' to call Gemini models
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    prompt=args.user_prompt
+# Now we can access `args.user_prompt`
     response = client.models.generate_content(
-    model='gemini-2.5-flash', contents=prompts)
+    model='gemini-2.5-flash', contents=prompt)
     p_token=response.usage_metadata.prompt_token_count
     r_token=response.usage_metadata.candidates_token_count
-    print(prompts)
+    print(prompt)
     print("Prompt tokens:",p_token)
     print("Response tokens:",r_token)
     print(response.text)

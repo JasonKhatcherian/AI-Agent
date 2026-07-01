b6777e0d04936946a45b3ed015ae7652fc939663
ThePrimeagen <the.primeagen@aol.com>
2026-07-01 11:43:52 +0300

    Implement secure file listing function

--- test_get_files_info.py
+++ test_get_files_info.py
@@ -1,6 +1,13 @@
 from functions.get_files_info import get_files_info
 
+print('get_files_info("calculator", "."):\nResult for current directory:')
 print(get_files_info("calculator", "."))
+
+print('\nget_files_info("calculator", "/bin"):\nResult for /bin:')
 print(get_files_info("calculator", "/bin"))
+
+print('\nget_files_info("calculator", "../"):\nResult for ../:')
 print(get_files_info("calculator", "../"))
+
+print('\nget_files_info("calculator", "main.py"):\nResult for main.py:')
 print(get_files_info("calculator", "main.py"))
